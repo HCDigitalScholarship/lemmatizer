a1755de5f2fc0e890772810f0fd72ed0c46b6fba
apjanco <ajanco@haverford.edu>
2017-08-18 11:55:30 -0400

    added tools to handle uploaded files

---
+++ utils.py
@@ -0,0 +1,2 @@
+#This is the utilities file for the Bridge Lemmatizer
+#It contains most of the backend code for lemmatization 
--- views.py
+++ views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import UploadFileForm
+from utils import handle_uploaded_file
+
 
 def lemmatizer(request):
 	return render(request,'lemmatizer.html')
  
-# Create your views here.
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
