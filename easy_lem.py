fe4a104140bf38f171cae7c93fd545bb45c0ba24
root <apjanco@gmail.com>
2017-09-08 17:49:48 +0000

    updated easy_lem

--- easy_lem.py
+++ easy_lem.py
@@ -1,22 +1,25 @@
 import os
 path = os.path.dirname(os.path.realpath(__file__))
 
-#Use autoLemma.py to create lemma sheet
-def lemmatize(language,filename):
+def lemmatize(language,filename,format):
     try: 
     
         os.system('python3 autoLemma.py {0} {1}'.format(language, filename))   
     
         #convert lemma format to Bridge
-        os.system('python3 convert_lemmata_format.py {0} import morpheus-bridge.xlsx'.format(language))   
+        if language == 'latin':
+            os.system('python3 convert_lemmata_format.py {0} import morpheus-bridge.xlsx'.format(language))   
+        elif language == 'greek':
+            os.system('python3 convert_lemmata_format.py {0} import Convert-bridge-morpheus-greek.xlsx'.format(language))   
 
         new_filename = filename.split('.')[0] + '.xlsx'
-        
-        os.system('python3 convert_lemmata_format.py {0} convert {1} morpheus bridge'.format(language,new_filename))
-        
-        #clean up spreadsheet for import into bridge
-        os.system('python3 format_lemmatized_text.py {0}'.format(new_filename))
-        
+        if format == 'bridge':
+            os.system('python3 convert_lemmata_format.py {0} convert {1} morpheus bridge'.format(language,new_filename))
+            os.system('python3 format_lemmatized_text.py {0}'.format(new_filename))
+
+        else:
+            pass
+         
     except:
         print ('Unable to lemmatize file.')        
-lemmatize('latin','latin_text.txt')
+lemmatize('greek','greek_text.txt','bridge')
