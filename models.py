394230266e289071c6992ac3c556810c66b64bdf
root <apjanco@gmail.com>
2017-09-21 16:36:13 +0000

    using Django model for form rather than HTML, old file saved as html_form_lemmatizer.html, files save file to /tmp, text is saved to db.

--- models.py
+++ models.py
@@ -1,8 +1,35 @@
 from django.db import models
+from django.core.files.storage import FileSystemStorage
+
 
 # Create your models here.
 
 class lemmmatizer(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+	LATIN = 'latin'
+	GREEK = 'greek'
+	LANGUAGE_CHOICES = ((LATIN, 'Latin'), (GREEK, 'Greek'))
+
+	language = models.CharField(max_length = 5, choices=LANGUAGE_CHOICES, default=LATIN)
+	
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	file = models.FileField(blank=True, storage=FileSystemStorage(location='/tmp'))
+	
+	text = models.TextField(default='')
+
+	ALL = 'all'
+	ONLY_UN = 'oun'
+	LEVEL_CHOICES = ((ALL,'Lemmatize All Forms'),(ONLY_UN,'Only Unambiguous Forms'))
+
+	lem_level = models.CharField(max_length = 22, choices=LEVEL_CHOICES, default=ALL)
+
+	BRIDGE = 'bridge'
+	MORPHEUS = 'morpheus'
+	FORMAT_CHOICES = ((BRIDGE, 'Bridge'), (MORPHEUS, 'Morpheus'))
+
+	lem_format = models.CharField(max_length = 8, choices=FORMAT_CHOICES, default=BRIDGE)
+    
+
+
+    # i would think you would add something about the language here... 
+    # later on if lemmatizer.language == 'latin': is asked   
--- views.py
+++ views.py
@@ -2,31 +2,38 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader, RequestContext
 from django.shortcuts import get_object_or_404, render, redirect
 from utils import handle_uploaded_file
-from lemmatizer.forms import PostText
+from lemmatizer.forms import PostText, post_text
+from lemmatizer import easy_lem
 
+#def lemmatizer(request):
+#	return render(request,'lemmatizer.html')
 
-
-def lemmatizer(request):
-	return render(request,'lemmatizer.html')
-
-def output_page(request):
-    return render(request,'lemmatized.html')
+#def lemmatized(request):
+#    test = "came from func output_page...interesting"
+#    return render(request,'lemmatized.html', test='hi')
 #def lemmatize(request):
 #	return render(request,'lemmatize.html')
 	#return render_to_response('fileupload/upload.html', {'form': c['UploadFileForm']},  RequestContext(request))
 
  
-def lemmatize_text(request):
+def lemmatizer(request):
+    
     if request.method == 'POST':
         form = PostText(request.POST, request.FILES)
+        
         if form.is_valid():
+
+            
             form.save(commit=True)
-            return render(request, 'lemmatized.html')
+
+            '''Here is where we call easy_lem.lemmatize()'''
+
+            return render(request, 'lemmatized.html',{'form':form})#,{'test'='hi1'})
         else:
             print(form.errors)
     else:
-        form = PostForm()
-    return render(request, 'lemmatized.html', {'form': form})
+        form = PostText()
+    return render(request, 'lemmatizer.html',{'form':form}) # {'form': form, 'test':'hi2'})
 
 def lemmatize_file(request):
     if request.method == 'POST':
@@ -36,5 +43,7 @@ def lemmatize_file(request):
             return HttpResponseRedirect('lemmatize.html')
     else:
         form = UploadFileForm()
+
+    return render(request, 'lemmatized.html', {'form': form,test:'hi4'})
     return render(request, 'lemmatize.html', {'form': form})
 
