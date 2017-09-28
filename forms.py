f85b65cb78b4bae4fafdc38b314a7e87b7309b97
root <apjanco@gmail.com>
2017-09-28 16:20:28 +0000

    forgot forms?

--- forms.py
+++ forms.py
@@ -1,9 +1,29 @@
 from django import forms
 from django.db import models
 from django.core.files.storage import FileSystemStorage
+import random
+
 
 
 from lemmatizer.models import lemmmatizer
+
+def romanMath():
+    correct = False
+    while (correct == False):
+        romanNum = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']
+        length = len(romanNum)
+        a = random.randint(1, length)
+        b = random.randint(1, length)
+        operation = random.randint(1, 2)
+        if operation == 1: 
+            question = romanNum[a - 1] + " + " + romanNum[b- 1]+ " = "
+            answer = a + b
+        elif operation == 2: 
+            question = romanNum[max(a, b) -1] + " - " + romanNum[min(a, b)- 1]+ " = "
+            answer = max(a, b) - min(a, b)
+        return question 
+        #print "Please solve the roman numeral math problem below, and give your answer in regular numbers."
+        
 
 class post_text(forms.Form):
     LATIN = 'latin'
@@ -23,9 +43,10 @@ class post_text(forms.Form):
     lem_format = models.CharField(max_length = 8, choices=FORMAT_CHOICES, default=BRIDGE)
 
 class PostText(forms.ModelForm):
+
 	class Meta:
 		model = lemmmatizer
-		fields = ['file','language','out_format','lem_format']
-        labels = {
-            'file': None
-        }
+		fields = ('file','language','out_format','lem_format','question')
+        file = forms.FileField(label='')
+        
+
